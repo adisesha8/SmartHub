1ac4cff2029a023df577b15218009848d2cb7809
Adisesha Manchala <AManchala@my.harrisburgu.edu>
2020-01-18 18:14:54 -0500

    Added attributes to Model Class and resized the images in Smart Center Services.

--- ModelProject/ModelClass.cs
+++ ModelProject/ModelClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,34 @@ namespace ModelProject
 {
     public class ModelClass
     {
+        [DisplayName("Salutation")]
+        public string Prefix { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Middle Name")]
+        public string MiddleName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Suffix")]
+        public string Suffix { get; set; }
+
+        [DisplayName("Gender")]
+        public string Sex { get; set; }
+
+        [DisplayName("Date Of Birth")]
+        public DateTime DOB { get; set; }
+
+        [DisplayName("Age")]
+        public string Age { get; set; }
+
+        [DisplayName("Locality")]
+        public string Locale { get; set; }
+
+        [DisplayName("Marital Status")]
+        public string MaritalStatus { get; set; }
     }
 }
